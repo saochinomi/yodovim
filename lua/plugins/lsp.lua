30a282ac2a2890659c3dc825ea2a068eb3b85959
saochinomi <299081003+saochinomi@users.noreply.github.com>
2026-08-10 20:45:00 +0500

    lsp для python и c/c++, автодополнение, форматирование

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,109 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    cmd = "Mason",
+    build = ":MasonUpdate",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    opts = {
+      ensure_installed = {
+        "black",
+        "ruff",
+        "clang-format",
+        "stylua",
+      },
+    },
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
+    config = function()
+      local on_attach = function(_, bufnr)
+        local opts = { buffer = bufnr }
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+        vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action, { buffer = bufnr, desc = "Code action" })
+        vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { buffer = bufnr, desc = "Rename" })
+        vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { buffer = bufnr, desc = "Diagnostics" })
+        vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+        vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+      end
+
+      local function capabilities()
+        local ok, blink = pcall(require, "blink.cmp")
+        if ok then
+          return blink.get_lsp_capabilities()
+        end
+        return vim.lsp.protocol.make_client_capabilities()
+      end
+
+      require("mason-lspconfig").setup({
+        ensure_installed = { "pyright", "clangd", "lua_ls" },
+        handlers = {
+          function(server)
+            require("lspconfig")[server].setup({
+              on_attach = on_attach,
+              capabilities = capabilities(),
+            })
+          end,
+        },
+      })
+
+      vim.diagnostic.config({
+        virtual_text = { prefix = "● " },
+        signs = true,
+        update_in_insert = false,
+      })
+    end,
+  },
+  {
+    "saghen/blink.cmp",
+    version = "*",
+    event = "InsertEnter",
+    dependencies = {
+      "L3MON4D3/LuaSnip",
+      "rafamadriz/friendly-snippets",
+    },
+    config = function()
+      require("luasnip.loaders.from_vscode").lazy_load()
+      require("blink.cmp").setup({
+        keymap = {
+          preset = "default",
+        },
+        sources = {
+          default = { "lsp", "path", "snippets", "buffer" },
+        },
+        signature = { enabled = true },
+        snippets = { preset = "luasnip" },
+        completion = {
+          documentation = { auto_show = true },
+        },
+      })
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    cmd = "ConformInfo",
+    opts = {
+      formatters_by_ft = {
+        python = { "black" },
+        c = { "clang_format" },
+        cpp = { "clang_format" },
+        lua = { "stylua" },
+      },
+      format_on_save = {
+        timeout_ms = 2000,
+        lsp_format = "fallback",
+      },
+    },
+  },
+  }
